d44383e00b3f14ee790f2c8a567fe5a23e4dd3f6
Ivkina Maria <ip213s09@bwp16.csc.local>
2023-02-07 16:14:07 +0700

    Change main.c again

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-<<<<<<< HEAD
-=======
-int main()
-{
-  printf("Hello, World!");
-  return 0;
-}
->>>>>>> 09d5900b714490af47acc5f3d82856ee4a52055b
 
 int main()
 {
     printf("Hello, World!");
     return 0;
 }
+
